f1e71d274e59b821c7531c090e9605ab558a908b
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2017-01-14 18:17:24 -0200

    improvements

---
+++ src/github.com/compufour/IBPT/Makefile
@@ -0,0 +1,9 @@
+all:
+	go build importer.go
+	go build main.go
+clean:
+	rm -rf *.db
+	rm -rf data
+	rm -rf *.csv
+	rm -rf a.out
+	rm -rf *.sql
---
+++ src/github.com/compufour/IBPT/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetProduto(t *testing.T) {
+	var expected = Produto{"70101000", "SC", 0, "Ampolas de vidro,p/transporte ou embalagem", 13.45, 17.00, 18.02, 0.00, "0", "01/01/2017", "30/06/2017", "W7m9E1", "17.1.A", "IBPT"}
+	var result, _ = GetProduto("70101000", "SC", "0")
+	if result != expected {
+		t.Error(
+			"expected", expected,
+			"got", result,
+		)
+	}
+}
+
+func TestGetProdutoNotFound(t *testing.T) {
+	expected := "Produto not found"
+	var _, err = GetProduto("7010110", "SC", "0")
+	if err.Error() != expected {
+		t.Error(
+			"expected", expected,
+			"got", err.Error(),
+		)
+	}
+}
+
+func TestGetProdutoNotFoundOnAPI(t *testing.T) {
+}
